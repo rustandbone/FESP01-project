75782b6c5ade6a9e93fc208bff97f331d4e277c3
rustandbone <dlehdgh14@naver.com>
2023-12-22 14:23:56 +0900

    extra? 수정

--- api/utils/priceUtil.js
+++ api/utils/priceUtil.js
@@ -6,19 +6,33 @@ import codeUtil from '#utils/codeUtil.js';
 import userModel from '#models/user/user.model.js';
 
 const priceUtil = {
-  async getCost( { user_id, products, clientDiscount = { products: 0, shippingFees: 0 } }){
-
+  async getCost({
+    user_id,
+    products,
+    clientDiscount = { products: 0, shippingFees: 0 },
+  }) {
     const sellerBaseShippingFees = {};
     const productArray = _.map(products, '_id');
-    const dbProducts = await db.product.find({ _id: { $in: productArray } }).toArray();
+    const dbProducts = await db.product
+      .find({ _id: { $in: productArray } })
+      .toArray();
 
     dbProducts.forEach((product) => {
       const beforeShippingFees = sellerBaseShippingFees[product.seller_id];
-      product.price = product.price * _.find(products, {_id: product._id}).quantity;
-      if(beforeShippingFees === undefined){
-        sellerBaseShippingFees[product.seller_id] = product.shippingFees === undefined ? global.config.shippingFees.value : product.shippingFees;
-      }else{
-        sellerBaseShippingFees[product.seller_id] = Math.max(beforeShippingFees, product.shippingFees === undefined ? global.config.shippingFees.value : product.shippingFees);
+      product.price =
+        product.price * _.find(products, { _id: product._id }).quantity;
+      if (beforeShippingFees === undefined) {
+        sellerBaseShippingFees[product.seller_id] =
+          product.shippingFees === undefined
+            ? global.config.shippingFees.value
+            : product.shippingFees;
+      } else {
+        sellerBaseShippingFees[product.seller_id] = Math.max(
+          beforeShippingFees,
+          product.shippingFees === undefined
+            ? global.config.shippingFees.value
+            : product.shippingFees
+        );
       }
     });
 
@@ -30,32 +44,47 @@ const priceUtil = {
 
     // 회원 등급별 할인율
     const totalDiscount = clientDiscount; // 상품 할인 쿠폰, 배송비 쿠폰 처럼 주문 정보에 포함된 할인 금액
-    if(user_id){
+    if (user_id) {
       // 회원 등급
-      const membershipClass = await userModel.findAttrById(user_id, 'extra.membershipClass');
+      const membershipClass = await userModel.findAttrById(
+        user_id,
+        'extra.membershipClass'
+      );
       // 회원 등급별 할인율
-      const discountRate = codeUtil.getCodeAttr(membershipClass?.extra.membershipClass, 'discountRate');
+      const discountRate = codeUtil.getCodeAttr(
+        membershipClass?.extra?.membershipClass,
+        'discountRate'
+      );
 
-      if(discountRate !== undefined){
-        totalDiscount.products = clientDiscount.products + Math.ceil((cost.products - clientDiscount.products) * (discountRate/100) /10) * 10;
+      if (discountRate !== undefined) {
+        totalDiscount.products =
+          clientDiscount.products +
+          Math.ceil(
+            ((cost.products - clientDiscount.products) * (discountRate / 100)) /
+              10
+          ) *
+            10;
       }
     }
 
     const result = {
       ...cost,
       discount: totalDiscount,
-      total: cost.products - totalDiscount.products
+      total: cost.products - totalDiscount.products,
     };
 
     // 무료 배송 확인
-    if(global.config.freeShippingFees?.value && (result.total >= global.config.freeShippingFees.value)){
+    if (
+      global.config.freeShippingFees?.value &&
+      result.total >= global.config.freeShippingFees.value
+    ) {
       result.discount.shippingFees = cost.shippingFees;
     }
 
     result.total += cost.shippingFees - result.discount.shippingFees;
     logger.debug(result);
     return result;
-  }
+  },
 };
 
 export default priceUtil;
